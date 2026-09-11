d4401ab8fc104844e576f06951172d4c50c1b051
Ponhari-S <ponhari9923@gmail.com>
2026-09-11 11:45:12 +0530

    Deployment Update

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "../features/auth/authSlice";
 import { useNavigate } from "react-router-dom";
